e3e9f0607f22b3ac8e146dd3141245ce9fb43203
nefolem <nef0.l3m@gmail.com>
2023-12-02 00:47:18 +0200

    add grid snap

---
+++ Assets/SnapToGrid.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class SnapToGrid : MonoBehaviour
+{
+
+    [SerializeField] float gridSize = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 snapPos;
+
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.y = 0f;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = snapPos;
+    }
+}
