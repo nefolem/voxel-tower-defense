1197adb2ef9480a94b89ab849ef85361e8848fe3
nefolem <nef0.l3m@gmail.com>
2023-12-02 01:21:15 +0200

    add coordinates display

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 
 [ExecuteInEditMode]
-public class SnapToGrid : MonoBehaviour
+public class CubeEditor : MonoBehaviour
 {
-
     [SerializeField] float gridSize = 10f;
+    TextMesh lebel;
     // Update is called once per frame
     void Update()
-    {
+    {        
         Vector3 snapPos;
 
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.y = 0f;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-
         transform.position = snapPos;
+
+        lebel = GetComponentInChildren<TextMesh>();
+        lebel.text = snapPos.x/gridSize + "," + snapPos.z/gridSize;
     }
 }
